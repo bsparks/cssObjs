40d10e45f2eefbbbd888f07b2105075f59c169b7
Ben Sparks <bsparks@unicon.net>
2011-05-06 10:57:17 -0700

    added trailing ; | added rotate function

--- cssObjs/cssobjs/extensions/cssobjs.css3.js
+++ cssObjs/cssobjs/extensions/cssobjs.css3.js
@@ -8,38 +8,47 @@
     /*=============== Use the $.cssObjsAPI to add the Functions ================*/
     $.cssObjsAPI.addFunctions({
         rounded_corners: function(params) {
-            return ["border-radius:", params, "-moz-border-radius:", params, "-webkit-border-radius:", params].join(';');
+            return ["border-radius:", params, "-moz-border-radius:", params, "-webkit-border-radius:", params].join(';') + ';';
         },
         leafUL: function(params) {
-            return ["border-top-left-radius:", params, "-moz-border-radius-topleft:", params, "border-bottom-right-radius:", params, "-moz-border-radius-bottomright:", params].join(';');
+            return ["border-top-left-radius:", params, "-moz-border-radius-topleft:", params, "border-bottom-right-radius:", params, "-moz-border-radius-bottomright:", params].join(';') + ';';
         },
         leafUR: function(params) {
-            return ["border-top-right-radius:", params, "-moz-border-radius-topright:", params, "border-bottom-left-radius:", params, "-moz-border-radius-bottomleft:", params].join(';');
+            return ["border-top-right-radius:", params, "-moz-border-radius-topright:", params, "border-bottom-left-radius:", params, "-moz-border-radius-bottomleft:", params].join(';') + ';';
         },
         tab: function(params) {
-            return ["-moz-border-radius-topright:", params, "border-top-right-radius:", params, "-moz-border-radius-topleft:", params, "border-top-left-radius:", params].join(';');
+            return ["-moz-border-radius-topright:", params, "border-top-right-radius:", params, "-moz-border-radius-topleft:", params, "border-top-left-radius:", params].join(';') + ';';
         },
         inner_shadow: function(params) {
 
-            var params = params.split(' ');
-            var offTop = params[0];
-            var offLeft = params[1];
-            var radius = params[2];
-            var color = params[3];
+            var params = params.split(' '),
+                offTop = params[0],
+                offLeft = params[1],
+                radius = params[2],
+                color = params[3];
 
-            return [["-webkit-box-shadow: inset", offTop, offLeft, radius, color].join(' '), ["-moz-box-shadow: inset", offTop, offLeft, radius, color].join(' '), ["box-shadow: inset", offTop, offLeft, radius, color].join(' ')].join(';');
+            return [["-webkit-box-shadow: inset", offTop, offLeft, radius, color].join(' '), ["-moz-box-shadow: inset", offTop, offLeft, radius, color].join(' '), ["box-shadow: inset", offTop, offLeft, radius, color].join(' ')].join(';') + ';';
 
         },
         box_shadow: function(params) {
 
-            var params = params.split(' ');
-            var offTop = params[0];
-            var offLeft = params[1];
-            var radius = params[2];
-            var color = params[3];
+            var params = params.split(' '),
+                offTop = params[0],
+                offLeft = params[1],
+                radius = params[2],
+                color = params[3];
 
             return [["-moz-box-shadow:", offTop, offLeft, radius, color].join(' '), ["-webkit-box-shadow:", offTop, offLeft, radius, color].join(' '), ["box-shadow:", offTop, offLeft, radius, color].join(' '), ["-ms-filter:progid:DXImageTransform.Microsoft.Shadow(Strength=", radius,
-                                     ",Direction=90,Color=\'", color, "\')"].join('')].join(';');
+                                                                                                     ",Direction=90,Color=\'", color, "\')"].join('')].join(';') + ';';
+        },
+        // TODO: IE6-IE9 support
+        rotate: function(params) {
+            // params should be single angle (default degrees)
+            return [["-moz-transform:rotate(", params, "deg)"].join(''), // FF 3.5+
+            ["-o-transform:rotate(", params, "deg)"].join(''), // Opera 10.5
+            ["-webkit-transform:rotate(", params, "deg)"].join(''), // Safari 3.1+, Chrome
+            ["-ms-transform:rotate(", params, "deg)"].join(''), // IE9
+            ["transform:rotate(", params, "deg)"].join('')].join(';') + ';';
         }
     });
 
