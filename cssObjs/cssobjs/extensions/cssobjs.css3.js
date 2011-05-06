b105bad6a81c5df5e694756662b67752c195f2ba
Ben Sparks <bsparks@unicon.net>
2011-05-06 12:45:15 -0700

    replaced "utils" with browsers prefix array; added transitions

--- cssObjs/cssobjs/extensions/cssobjs.css3.js
+++ cssObjs/cssobjs/extensions/cssobjs.css3.js
@@ -3,7 +3,7 @@
     //CSS3 Extension using cssObjs functions
     //Adds functions that manipulate css values
     /*=============== Private Utility Functions ================*/
-    var utils = {};
+    var browsers = ["-moz-", "-webkit-", "-ms-", "-o-", ""];
 
     /*=============== Use the $.cssObjsAPI to add the Functions ================*/
     $.cssObjsAPI.addFunctions({
@@ -39,7 +39,7 @@
                 color = params[3];
 
             return [["-moz-box-shadow:", offTop, offLeft, radius, color].join(' '), ["-webkit-box-shadow:", offTop, offLeft, radius, color].join(' '), ["box-shadow:", offTop, offLeft, radius, color].join(' '), ["-ms-filter:progid:DXImageTransform.Microsoft.Shadow(Strength=", radius,
-                                                                                                     ",Direction=90,Color=\'", color, "\')"].join('')].join(';') + ';';
+                                                                                                                                                                     ",Direction=90,Color=\'", color, "\')"].join('')].join(';') + ';';
         },
         // TODO: IE6-IE9 support
         rotate: function(params) {
@@ -49,6 +49,41 @@
             ["-webkit-transform:rotate(", params, "deg)"].join(''), // Safari 3.1+, Chrome
             ["-ms-transform:rotate(", params, "deg)"].join(''), // IE9
             ["transform:rotate(", params, "deg)"].join('')].join(';') + ';';
+        },
+        // css3 transitions
+        transition: function(params) {
+            var p = params.split(" "),
+                property = p[0],
+                duration = p[1],
+                timing_function = p[2],
+                pStr, dStr, tStr, result, goLong = false;
+
+            // allow properties to be comma delim
+            if (property.split(',').length > 1 || duration.split(',').length > 1 || timing_function.split(',').length > 1) {
+                goLong = true; // we need to go long hand from now on
+            }
+
+            if (goLong) {
+                pStr = $.map(browsers, function(b) {
+                    return [[b, "transition-property:"].join(''), property].join(' ')
+                }).join(';\n') + ';\n';
+
+                dStr = $.map(browsers, function(b) {
+                    return [[b, "transition-duration:"].join(''), duration].join(' ')
+                }).join(';\n') + ';\n';
+
+                tStr = $.map(browsers, function(b) {
+                    return [[b, "transition-timing-function:"].join(''), timing_function].join(' ')
+                }).join(';\n') + ';\n';
+
+                result = [pStr, dStr, tStr].join('\n');
+            } else {
+                result = $.map(browsers, function(b) {
+                    return [[b, "transition:"].join(''), property, duration, timing_function].join(' ');
+                }).join(';\n') + ';\n';
+            }
+
+            return result;
         }
     });
 
